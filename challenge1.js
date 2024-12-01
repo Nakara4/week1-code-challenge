ce038c5b04c0e9eeeae069450e93c3a278a878dd
Dennis Karanja <ngangapepe8@gmail.com>
2024-12-01 15:29:06 +0300

    Adds the if NaN

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,14 @@
-function getStudentGrade() {}
-// Promot the user for marks
-let marks = prompt("Enter the student's marks (0-100):");
+function getStudentGrade() {// Promot the user for marks
+    let marks = prompt("Enter the student's marks (0-100):");
+    
+    // Convert the input to a number
+    marks = Number(markes);
+    
+    // Validate the input
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+        alart("Invalid input. Please enter a number between 0 and 100.");
+        return;
+    
+    }
+
+}
