d60e1da43237f14aa24c811ceae601fa83dd145d
Dennis Karanja <ngangapepe8@gmail.com>
2024-12-01 16:43:17 +0300

    Adds if NaN

--- challenge2.js
+++ challenge2.js
@@ -1,4 +1,13 @@
 function checkSpeed() {
-    //prompt the user to input the speed of the car
+    // prompt the user to input the speed of the car
     let speed = prompt("Enter the speed of the car (km/h):");
+
+    // Convert the input to a number
+    speed = Number(speed);
+
+    // validate the input
+    if (isNaN(speed) || speed < 0) {
+        alert("Invalid input. Please enter a valid speed.");
+        return;
+    }
 }
