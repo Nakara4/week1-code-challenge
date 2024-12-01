a3e4ea67a821810d8c6b3f3094437a334527baa3
Dennis Karanja <ngangapepe8@gmail.com>
2024-12-01 15:42:22 +0300

    Adds if else

--- challenge1.js
+++ challenge1.js
@@ -11,4 +11,20 @@ function getStudentGrade() {// Promot the user for marks
     
     }
 
+    // Determine the grade
+    let grade;
+    if  (marks > 79) {
+        grade = "A";
+    } else if (marks >= 60) {
+        grade = "B";
+    } else if (marks >= 50) {
+        grade = "C"; 
+    } else if (marks >+ 40) {
+        grade = "D";
+    }else {
+        grade = "E";
+    }
+
+
+
 }
